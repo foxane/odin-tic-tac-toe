ae7a6d61a2a5abf10613faba76754817d77c3d6a
foxane <65512187+foxane@users.noreply.github.com>
2024-05-11 13:12:34 +0700

    Added factory functions for player and board

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
+"use strict";
+
 const Player = function (name, marker) {
-  const setName = function (newName) {
-    name = newName;
-  };
-  return { name, marker, setName };
+  return { name, marker };
 };
 
 const Board = function () {
-  let emptyBoard = [];
-  for (let i = 0; i < 9; i++) {
-    emptyBoard.push("");
-  }
+  const gameBoard = Array(9);
   const winCon = [
     [0, 1, 2],
     [3, 4, 5],
@@ -20,104 +16,43 @@ const Board = function () {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  const set = function (pos) {
-    if (this.gameBoard[pos]) return;
-    this.gameBoard[pos] = this.player[this.turn].marker;
+  // Untested
+  const reset = function () {
+    gameBoard = emptyBoard;
   };
-  const checkWinner = function () {
-    for (let i = 0; i < this.winCon.length; i++) {
-      const condition = this.winCon[i];
-      const [a, b, c] = [
-        this.gameBoard[condition[0]],
-        this.gameBoard[condition[1]],
-        this.gameBoard[condition[2]],
-      ];
-      if (a === b && b === c) {
-        win = true;
+  // Working
+  const addMarker = function (pos, marker) {
+    if (gameBoard[pos]) return;
+    gameBoard[pos] = marker;
+  };
+  // Working
+  const checkWin = function () {
+    let isWin = false;
+    for (let i = 0; i < winCon.length; i++) {
+      const condition = winCon[i];
+      const a = gameBoard[condition[0]];
+      const b = gameBoard[condition[1]];
+      const c = gameBoard[condition[2]];
+      if (a === undefined || b === undefined || c === undefined) continue;
+      if (a === b && b === c) isWin = true;
+    }
+    return isWin;
+  };
+  // Working perfectly
+  const checkDraw = function () {
+    let isDraw = false;
+    for (const el of gameBoard) {
+      if (el === undefined) {
+        isDraw = false;
+        break;
+      } else {
+        isDraw = true;
       }
     }
-    // Found winner
-    if (win) {
-      this.winner = this.player[this.turn].name;
-      console.log(this.winner + " win");
-      this.state = "over";
-    } else if (!this.gameBoard.includes("")) {
-      this.state = "draw";
-    }
+    return isDraw;
   };
-  return { emptyBoard, winCon, set, checkWinner };
-};
-
-const State = function () {
-  const status = "running";
-  const winner = "";
-  const turn = 0;
-  return { status, winner, turn };
-};
-
-const players = [Player("player1", "x"), Player("player2", "o")];
-const board = Board();
-const state = State();
-console.log(board);
-console.log(state);
-console.log(players[1]);
-
-/* 
-(function () {
-  const game = {
-    player: [(p1 = Player("foc", "x")), (p2 = Player("wol", "o"))],
-    gameBoard: Board(),
-    turn: 0,
-    state: "running",
-    winner: "",
-    winCon: [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ],
-    reset: function () {
-      this.turn = 0;
-      this.winner = "";
-      this.gameBoard = Board();
-      this.state = "running";
-    },
-    set: function (pos) {
-      if (this.gameBoard[pos]) return;
-      this.gameBoard[pos] = this.player[this.turn].marker;
-      this.turn ? this.turn-- : this.turn++;
-    },
-    checkWinner: function () {
-      let win = false;
-
-      for (let i = 0; i < this.winCon.length; i++) {
-        const condition = this.winCon[i];
-        const [a, b, c] = [
-          this.gameBoard[condition[0]],
-          this.gameBoard[condition[1]],
-          this.gameBoard[condition[2]],
-        ];
-        if (a === "" || b === "" || c === "") {
-          continue;
-        }
-        if (a === b && b === c) {
-          win = true;
-        }
-      }
-      // Found winner
-      if (win) {
-        this.winner = this.player[this.turn].name;
-        console.log(this.winner + " win");
-        this.state = "over";
-      } else if (!this.gameBoard.includes("")) {
-        this.state = "draw";
-      }
-    },
+  const debug = function () {
+    console.log(gameBoard);
   };
-  const dom = {};
-})();
- */
+  return { reset, addMarker, checkWin, checkDraw, debug };
+};
