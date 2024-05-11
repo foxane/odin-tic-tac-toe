31c7777bb50181be0d89fc0c963bb7f2baa4f724
foxane <65512187+foxane@users.noreply.github.com>
2024-05-11 20:10:41 +0700

    Added bones for gameController

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 const createPlayer = function (name, marker) {
   const getName = () => name;
   const getMarker = () => marker;
-  return { name, marker, getName, getMarker };
+  return { getName, getMarker };
 };
 
-const gameboard = (function () {
-  let board = Array(9);
+const gameBoard = (function () {
+  let board;
   const WINCON = [
     [0, 1, 2],
     [3, 4, 5],
@@ -19,15 +19,20 @@ const gameboard = (function () {
     [2, 4, 6],
   ];
 
-  const reset = () => (board = Array(9));
+  const resetBoard = () => (board = Array.from({ length: 9 }, () => null));
   const getBoard = () => board;
-  const addMarker = (pos, mark) => (board[pos] = mark);
-  const checkDraw = () => board.every((el) => el !== undefined);
+  const addMarker = (pos, mark) => {
+    if (board[pos]) return;
+    board[pos] = mark;
+    checkDraw();
+    checkWin();
+  };
+  const checkDraw = () => board.every((el) => el !== null);
 
   const checkWin = () => {
     for (const [a, b, c] of WINCON) {
       if (
-        board[a] !== undefined &&
+        board[a] !== null && // Empty cell wont be counted
         board[a] === board[b] &&
         board[b] === board[c]
       ) {
@@ -37,26 +42,75 @@ const gameboard = (function () {
     return false;
   };
 
-  return { reset, getBoard, addMarker, checkDraw, checkWin };
+  return { resetBoard, getBoard, addMarker, checkDraw, checkWin };
 })();
 
-// const Game = function () {
-//   let status = "";
-//   const score = [0, 0];
-//   let turn = 0;
-//   const setStatus = function (newStatus) {
-//     status = newStatus;
-//   };
-//   const addScore = function () {
-//     score[turn]++;
-//     console.log(score);
-//   };
-//   const changeTurn = function () {
-//     turn === 0 ? turn++ : turn--;
-//     console.log(turn);
-//   };
-//   const resetScore = function () {
-//     score = [0, 0];
-//   };
-//   return { setStatus, addScore, changeTurn, resetScore };
-// };
+const gameState = (function () {
+  const score = [0, 0];
+  let status = "running";
+  let turn = 0;
+
+  const getTurn = () => turn;
+  const switchTurn = () => (turn === 0 ? turn++ : turn--);
+  const addScore = (winner) => score[winner]++;
+  const getScore = () => score;
+  const setStatus = (newStatus) => (status = newStatus);
+
+  return { getTurn, addScore, switchTurn, setStatus, getScore };
+})();
+
+const gameController = (function (gameBoard, gameState, player1, player2) {
+  // Initialize game
+  const startGame = () => {
+    gameBoard.resetBoard();
+    gameState.setStatus("running");
+
+    console.log("Game Started");
+    playTurn();
+  };
+
+  const playTurn = () => {
+    const currentPlayer = getCurrentPlayer();
+    // Call handle input
+    gameBoard.addMarker(
+      prompt(`${currentPlayer.getName()} move, input marker position:`),
+      currentPlayer.getMarker()
+    );
+
+    console.log(gameBoard.getBoard());
+    if (gameBoard.checkWin()) {
+      handleWin(currentPlayer);
+    } else if (gameBoard.checkDraw()) {
+      handleDraw();
+    } else {
+      switchTurn();
+      playTurn();
+    }
+  };
+
+  const getCurrentPlayer = () =>
+    gameState.getTurn() === 0 ? player1 : player2;
+  const switchTurn = () => gameState.switchTurn();
+
+  const handleWin = (winner) => {
+    gameState.addScore(winner === player1 ? 0 : 1);
+    console.log(
+      `${winner.getName()} wins! Score: ${gameState.getScore[0]}-${
+        gameState.getScore[1]
+      }`
+    );
+  };
+
+  const handleDraw = () => {
+    console.log("It's a draw!");
+  };
+
+  // TODO: create handle input to check wether the position selected is valid
+
+  startGame();
+})(
+  gameBoard,
+  gameState,
+  createPlayer("Player 1", "x"),
+  createPlayer("Player 2", "o")
+);
