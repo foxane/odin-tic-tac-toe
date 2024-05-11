3f0198871916d00f18245c776b35076435a8d11a
foxane <65512187+foxane@users.noreply.github.com>
2024-05-11 18:35:15 +0700

    Refactored player and gameboard

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 "use strict";
 
-const Player = function (name, marker) {
-  return { name, marker };
+const createPlayer = function (name, marker) {
+  const getName = () => name;
+  const getMarker = () => marker;
+  return { name, marker, getName, getMarker };
 };
 
-const Board = function () {
-  const gameBoard = Array(9);
-  const winCon = [
+const gameboard = (function () {
+  let board = Array(9);
+  const WINCON = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -16,89 +18,45 @@ const Board = function () {
     [0, 4, 8],
     [2, 4, 6],
   ];
-  // Untested
-  const reset = function () {
-    gameBoard = emptyBoard;
-  };
-  // Working
-  const addMarker = function (pos, marker) {
-    if (gameBoard[pos]) return;
-    gameBoard[pos] = marker;
-  };
-  // Working
-  const checkWin = function () {
-    let isWin = false;
-    for (let i = 0; i < winCon.length; i++) {
-      const condition = winCon[i];
-      const a = gameBoard[condition[0]];
-      const b = gameBoard[condition[1]];
-      const c = gameBoard[condition[2]];
-      if (a === undefined || b === undefined || c === undefined) continue;
-      if (a === b && b === c) isWin = true;
-    }
-    return isWin;
-  };
-  // Working perfectly
-  const checkDraw = function () {
-    let isDraw = false;
-    for (const el of gameBoard) {
-      if (el === undefined) {
-        isDraw = false;
-        break;
-      } else {
-        isDraw = true;
+
+  const reset = () => (board = Array(9));
+  const getBoard = () => board;
+  const addMarker = (pos, mark) => (board[pos] = mark);
+  const checkDraw = () => board.every((el) => el !== undefined);
+
+  const checkWin = () => {
+    for (const [a, b, c] of WINCON) {
+      if (
+        board[a] !== undefined &&
+        board[a] === board[b] &&
+        board[b] === board[c]
+      ) {
+        return true;
       }
     }
-    return isDraw;
+    return false;
   };
-  const debug = function () {
-    console.log(gameBoard);
-  };
-  return { reset, addMarker, checkWin, checkDraw, debug };
-};
 
-const Game = function () {
-  let status = "";
-  const score = [0, 0];
-  let turn = 0;
-  const setStatus = function (newStatus) {
-    status = newStatus;
-  };
-  const addScore = function () {
-    score[turn]++;
-    console.log(score);
-  };
-  const changeTurn = function () {
-    turn === 0 ? turn++ : turn--;
-    console.log(turn);
-  };
-  const resetScore = function () {
-    score = [0, 0];
-  };
-  return { setStatus, addScore, changeTurn, resetScore };
-};
-
-const DOM = function () {
-  const cells = document.querySelectorAll(".cell");
-  const render = function () {
-    // for (const [i, val] of board.gameBoard.entries()) {
-    //   cells[i].innerText = "awdwadal";
-    // }
-  };
-  return { cells, render };
-};
-
-const currentGame = (function () {
-  const player = [Player("player 1", "x"), Player("player 2", "o")];
-  const board = Board();
-  const game = Game();
-  const dom = DOM();
-  const runtime = { ...board, ...game, ...dom };
-  runtime.cells.forEach((cell) => {
-    cell.addEventListener("click", function () {
-      const pos = Number(cell.getAttribute("id"));
-      this.addMarker(pos, player[this.turn].marker).bind(this);
-    });
-  });
-  console.log(player[runtime.turn].marker);
+  return { reset, getBoard, addMarker, checkDraw, checkWin };
 })();
+
+// const Game = function () {
+//   let status = "";
+//   const score = [0, 0];
+//   let turn = 0;
+//   const setStatus = function (newStatus) {
+//     status = newStatus;
+//   };
+//   const addScore = function () {
+//     score[turn]++;
+//     console.log(score);
+//   };
+//   const changeTurn = function () {
+//     turn === 0 ? turn++ : turn--;
+//     console.log(turn);
+//   };
+//   const resetScore = function () {
+//     score = [0, 0];
+//   };
+//   return { setStatus, addScore, changeTurn, resetScore };
+// };
