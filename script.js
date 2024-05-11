596ce23b4e658ecc95e49680ec66e7f71de4b408
foxane <65512187+foxane@users.noreply.github.com>
2024-05-11 09:39:22 +0700

    Updated diagram

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const Player = function (name, marker) {
-  return { name, marker };
+  const setName = function (newName) {
+    name = newName;
+  };
+  return { name, marker, setName };
 };
 
 const Board = function () {
@@ -7,9 +10,59 @@ const Board = function () {
   for (let i = 0; i < 9; i++) {
     emptyBoard.push("");
   }
-  return emptyBoard;
+  const winCon = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  const set = function (pos) {
+    if (this.gameBoard[pos]) return;
+    this.gameBoard[pos] = this.player[this.turn].marker;
+  };
+  const checkWinner = function () {
+    for (let i = 0; i < this.winCon.length; i++) {
+      const condition = this.winCon[i];
+      const [a, b, c] = [
+        this.gameBoard[condition[0]],
+        this.gameBoard[condition[1]],
+        this.gameBoard[condition[2]],
+      ];
+      if (a === b && b === c) {
+        win = true;
+      }
+    }
+    // Found winner
+    if (win) {
+      this.winner = this.player[this.turn].name;
+      console.log(this.winner + " win");
+      this.state = "over";
+    } else if (!this.gameBoard.includes("")) {
+      this.state = "draw";
+    }
+  };
+  return { emptyBoard, winCon, set, checkWinner };
 };
 
+const State = function () {
+  const status = "running";
+  const winner = "";
+  const turn = 0;
+  return { status, winner, turn };
+};
+
+const players = [Player("player1", "x"), Player("player2", "o")];
+const board = Board();
+const state = State();
+console.log(board);
+console.log(state);
+console.log(players[1]);
+
+/* 
 (function () {
   const game = {
     player: [(p1 = Player("foc", "x")), (p2 = Player("wol", "o"))],
@@ -67,3 +120,4 @@ const Board = function () {
   };
   const dom = {};
 })();
+ */
