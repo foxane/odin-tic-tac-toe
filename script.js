6ef61b14e8466aa0f56279c49aa728f69690575f
foxane <65512187+foxane@users.noreply.github.com>
2024-05-12 14:40:31 +0700

    Console game working as expected

--- script.js
+++ script.js
@@ -21,12 +21,7 @@ const gameBoard = (function () {
 
   const resetBoard = () => (board = Array.from({ length: 9 }, () => null));
   const getBoard = () => board;
-  const addMarker = (pos, mark) => {
-    if (board[pos]) return;
-    board[pos] = mark;
-    checkDraw();
-    checkWin();
-  };
+  const addMarker = (pos, mark) => (board[pos] = mark);
   const checkDraw = () => board.every((el) => el !== null);
 
   const checkWin = () => {
@@ -59,58 +54,79 @@ const gameState = (function () {
   return { getTurn, addScore, switchTurn, setStatus, getScore };
 })();
 
-const gameController = (function (gameBoard, gameState, player1, player2) {
-  // Initialize game
+const moveHandler = (function () {
+  const isValid = function (pos) {
+    if (gameBoard.getBoard()[pos] === null) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  return { isValid };
+})();
+
+const gameController = (function (
+  gameBoard,
+  gameState,
+  moveHandler,
+  player1,
+  player2
+) {
+  // Module functions
+  const handleOutput = () => console.log(gameBoard.getBoard());
+  const handleWin = (player) => {
+    console.log(`${player} wins!`);
+    gameState.setStatus("over");
+    handleGameOver();
+  };
+  const handleDraw = () => {
+    console.log(`Draw!`);
+    handleGameOver();
+  };
+  const handleGameOver = () => {
+    if (confirm("Play Again?")) {
+      startGame();
+    }
+  };
+
+  // Initialize Game
   const startGame = () => {
     gameBoard.resetBoard();
     gameState.setStatus("running");
+    gameState.getTurn() === 0 ? "" : gameState.switchTurn();
+    // To be filled with dom
 
-    console.log("Game Started");
     playTurn();
   };
 
   const playTurn = () => {
-    const currentPlayer = getCurrentPlayer();
-    // Call handle input
-    gameBoard.addMarker(
-      prompt(`${currentPlayer.getName()} move, input marker position:`),
-      currentPlayer.getMarker()
+    const currentPlayer = gameState.getTurn() === 0 ? player1 : player2;
+    const movePosition = Number(
+      prompt(`${currentPlayer.getName()} turn, input position:`)
     );
+    // Input validation,
+    moveHandler.isValid(movePosition)
+      ? gameBoard.addMarker(movePosition, currentPlayer.getMarker())
+      : playTurn();
 
-    console.log(gameBoard.getBoard());
+    // Render board
+    handleOutput();
     if (gameBoard.checkWin()) {
-      handleWin(currentPlayer);
+      // Dom win
+      handleWin(currentPlayer.getName());
     } else if (gameBoard.checkDraw()) {
       handleDraw();
     } else {
-      switchTurn();
+      gameState.switchTurn();
       playTurn();
     }
   };
-
-  const getCurrentPlayer = () =>
-    gameState.getTurn() === 0 ? player1 : player2;
-  const switchTurn = () => gameState.switchTurn();
-
-  const handleWin = (winner) => {
-    gameState.addScore(winner === player1 ? 0 : 1);
-    console.log(
-      `${winner.getName()} wins! Score: ${gameState.getScore[0]}-${
-        gameState.getScore[1]
-      }`
-    );
-  };
-
-  const handleDraw = () => {
-    console.log("It's a draw!");
-  };
-
-  // TODO: create handle input to check wether the position selected is valid
 
   startGame();
 })(
   gameBoard,
   gameState,
+  moveHandler,
   createPlayer("Player 1", "x"),
   createPlayer("Player 2", "o")
 );
