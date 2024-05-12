a9eceeae97cbc6abe002969a82b31ee38d918778
foxane <65512187+foxane@users.noreply.github.com>
2024-05-12 21:29:37 +0700

    UPdated dom handler

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const createPlayer = function (name, marker) {
   const getMarker = () => marker;
   return { getName, getMarker };
 };
+
 const gameBoard = (function () {
   let board;
   const winCon = [
@@ -20,6 +21,7 @@ const gameBoard = (function () {
     [2, 4, 6],
   ];
 
+  // Gameboard methods
   const getBoard = () => board;
   const resetBoard = () => (board = Array.from({ length: 9 }, () => null));
   const addMarker = (pos, mark) => (board[pos] = mark);
@@ -37,6 +39,47 @@ const gameBoard = (function () {
     return false;
   };
 
-  return { getBoard, resetBoard, addMarker, checkDraw, checkDraw };
+  return { getBoard, resetBoard, addMarker, checkDraw, checkDraw, checkWin };
 })();
-console.log(gameBoard);
+
+const gameHandler = function () {
+  // PLayer will be indexed based on turn
+  let turn = 0;
+  let status = "running";
+  const score = [0, 0];
+
+  const resetTurn = () => (turn = 0);
+  const switchTurn = () => (turn === 0 ? turn++ : turn--);
+  const setStatus = (newStatus) => (status = newStatus);
+  const addScore = (winnerIndex) => score[winnerIndex]++;
+  const handleDraw = () => console.log("draw");
+  const handleWin = () => console.log("win");
+
+  return { resetTurn, switchTurn, setStatus, addScore, handleDraw, handleWin };
+};
+
+const domHandler = function () {
+  const cells = document.querySelectorAll(".cell");
+
+  const inputControl = (position, marker) => {
+    if (gameBoard.getBoard()[position] === null) {
+      gameBoard.addMarker(position, "x");
+      render();
+      console.log(gameBoard.getBoard());
+    }
+  };
+
+  const render = () => {
+    for (const [i, mark] of gameBoard.getBoard().entries()) {
+      cells[i].style.backgroundImage = `url(./images/${mark}.png)`;
+    }
+  };
+
+  cells.forEach((cell) => {
+    cell.addEventListener("click", function (e) {
+      inputControl(Number(e.target.getAttribute("id")));
+    });
+  });
+};
+gameBoard.resetBoard();
+domHandler();
